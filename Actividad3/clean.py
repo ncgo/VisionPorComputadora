38b6da0bef0bc159839b45f4ca598ee576f2e3d6
ncgo <ngarciaorozco@outlook.com>
2021-05-12 17:59:23 -0600

    Cleaned up data set for 140, 40, 20 distribution on each class

---
+++ Actividad3/clean.py
@@ -0,0 +1,33 @@
+import os
+from random import sample
+
+def deleteExtra(folder, className):
+    path = 'data/' + folder + '/' + className
+    files = os.listdir(path)
+    os.chdir(path)
+    print(os.getcwd())
+    if folder == 'train':
+        size = 140
+    elif folder == 'test':
+        size = 40
+    elif folder == 'validation':
+        size = 20
+    for file in sample(files,len(files) - size):
+        os.remove(file)
+
+def checkCleanUp():
+    folders = ['train', 'test', 'validation']
+    clases = ['airplane', 'bicycle', 'bus', 'car', 'motorcycle']
+
+    for className in clases:
+        print("className")
+        for folder in folders:
+            path = path = 'data/' + folder + '/' + className
+            files = os.listdir(path)
+            print(folder + ": " + len(files))
+
+if __name__ == "__main__":
+    # deleteExtra('validation', 'airplane')
+    # deleteExtra('train', 'car')
+    deleteExtra('validation', 'car')
+    checkCleanUp()
